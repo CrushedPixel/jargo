4eb00160fa9b6f9daf82c73eff7ddf8dc620aff6
CrushedPixel <crushedpixelmaps@gmail.com>
2018-03-11 23:48:18 +0100

    Formulated integration tests for nullable attributes

--- test/integration/nullable_test.go
+++ test/integration/nullable_test.go
@@ -7,14 +7,44 @@ import (
 	"testing"
 )
 
+type nullableAttribute struct {
+	Id   int64
+	Name *string
+	Age  *int
+}
+
 type nullableRelation struct {
 	Id       int64
-	Relation *dummy `jargo:",belongsTo,nullable"`
+	Relation *dummy `jargo:",belongsTo"`
 }
 
 type nonNullableRelation struct {
 	Id       int64
-	Relation *dummy `jargo:",belongsTo"`
+	Relation dummy `jargo:",belongsTo"`
+}
+
+type nullableIdField struct {
+	Id *int64 // this is invalid
+}
+
+// TestNullableAttributes tests the behaviour of nullable attribute fields.
+func TestNullableAttributes(t *testing.T) {
+	resource, err := app.RegisterResource(nullableAttribute{})
+	require.Nil(t, err)
+
+	// insert instance with attribute set to null
+	age := 20
+	_, err = resource.InsertInstance(app.DB(), &nullableAttribute{
+		Name: nil,
+		Age:  &age,
+	}).Result()
+	require.Nil(t, err)
+}
+
+// TestNullableIdFields tests the behaviour of nullable id fields (which is invalid).
+func TestNullableIdFields(t *testing.T) {
+	_, err := app.RegisterResource(nullableIdField{})
+	require.EqualError(t, err, "id field must not be nullable")
 }
 
 // TestNullableRelations tests the behaviour of nullable relation fields.
@@ -42,7 +72,7 @@ func TestNonNullableRelations(t *testing.T) {
 
 	// insert nonNullableRelation instance with relation set to value
 	res, err := resource.InsertInstance(app.DB(),
-		&nonNullableRelation{Relation: dummyInstance}).
+		&nonNullableRelation{Relation: *dummyInstance}).
 		Result()
 	require.Nil(t, err)
 	require.Equal(t, res.(*nonNullableRelation).Relation.Id, dummyInstance.Id)
